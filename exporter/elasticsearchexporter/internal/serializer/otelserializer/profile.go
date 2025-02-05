158ba593a29a0c4a7f9cb756b5096200c47c5060
Tim Rühsen <tim.ruhsen@elastic.co>
2025-02-05 12:04:51 +0100

    Add downsampling for profiling events

--- exporter/elasticsearchexporter/internal/serializer/otelserializer/profile.go
+++ exporter/elasticsearchexporter/internal/serializer/otelserializer/profile.go
@@ -22,52 +22,45 @@ const (
 
 // SerializeProfile serializes a profile and calls the `pushData` callback for each generated document.
 func SerializeProfile(resource pcommon.Resource, scope pcommon.InstrumentationScope, profile pprofile.Profile, pushData func(*bytes.Buffer, string, string) error) error {
+	pushDataAsJSON := func(data any, id, index string) error {
+		c, err := toJSON(data)
+		if err != nil {
+			return err
+		}
+		return pushData(c, id, index)
+	}
+
 	data, err := serializeprofiles.Transform(resource, scope, profile)
 	if err != nil {
 		return err
 	}
 
 	for _, payload := range data {
-		if payload.StackTraceEvent.StackTraceID != "" {
-			c, err := toJSON(payload.StackTraceEvent)
-			if err != nil {
+		event := payload.StackTraceEvent
+
+		if event.StackTraceID != "" {
+			if err = pushDataAsJSON(event, "", allEventsIndex); err != nil {
 				return err
 			}
-			err = pushData(c, "", allEventsIndex)
-			if err != nil {
+			if err = serializeprofiles.IndexDownsampledEvent(event, pushDataAsJSON); err != nil {
 				return err
 			}
 		}
 
 		if payload.StackTrace.DocID != "" {
-			c, err := toJSON(payload.StackTrace)
-			if err != nil {
-				return err
-			}
-			err = pushData(c, payload.StackTrace.DocID, stackTraceIndex)
-			if err != nil {
+			if err = pushDataAsJSON(payload.StackTrace, payload.StackTrace.DocID, stackTraceIndex); err != nil {
 				return err
 			}
 		}
 
 		for _, stackFrame := range payload.StackFrames {
-			c, err := toJSON(stackFrame)
-			if err != nil {
-				return err
-			}
-			err = pushData(c, stackFrame.DocID, stackFrameIndex)
-			if err != nil {
+			if err = pushDataAsJSON(stackFrame, stackFrame.DocID, stackFrameIndex); err != nil {
 				return err
 			}
 		}
 
 		for _, executable := range payload.Executables {
-			c, err := toJSON(executable)
-			if err != nil {
-				return err
-			}
-			err = pushData(c, executable.DocID, executablesIndex)
-			if err != nil {
+			if err = pushDataAsJSON(executable, executable.DocID, executablesIndex); err != nil {
 				return err
 			}
 		}
